0f0e1919c12bc54f87e32cfe09f069836d3ea0f6
{自分の名前} <misaki.ishihara0004@gmail.com>
2023-05-19 16:06:04 +0900

    postDataにバグあり

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 
+
 const name = "Ishihara Tech"
 
 function Layout({ children }) {
@@ -13,7 +14,7 @@ function Layout({ children }) {
                 <h1 className="text-5xl py-5 px-5">
                     <Link href="/">{name}</Link>
                 </h1>
-                <Link href="components/about">
+                <Link href="./pages/About">
                     <img className="object-cover h-30 w-24 py-3 mr-6" src="/images/ishihara.jpg" />
                 </Link>
             </header>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,31 @@
 import Layout from "../../components/Layout";
+import { getAllPostsIds, getPostData, } from "../../lib/posts";
 
-export default function Post() {
+export async function getStaticPaths() {
+    const paths = getAllPostsIds();
+
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id);
+
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+export default function Post( postData ) {
     return (
         <Layout>
-            <p>動的ルーティング</p>
+            {postData.title}
+            {postData.date}
+            {postData.blogContentHTML}
         </Layout>
     );
 };
