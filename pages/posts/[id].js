a00009beb541c826787560c99866673431769d30
{自分の名前} <misaki.ishihara0004@gmail.com>
2023-05-21 16:13:48 +0900

    /pages/posts/[id].jsのエラー解消(PostData function)

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,36 +1,77 @@
+// import { useRouter } from "next/router";
+// import { getAllPostsIds, getPostsData } from "../../lib/posts";
+
+// export default function PostData({ post }) {
+//     const router = useRouter();
+
+//     if(router.isFallback || !post) {
+//         return <div>Loading ...</div>
+//     }
+
+//     return (
+//         <>
+//             <h1>{post.title}</h1>
+//             <p>{post.created_at}</p>
+//             <p>{post.content}</p>
+//         </>
+//     );
+// }
+
+// export async function getStaticPaths() {
+//     const paths = await getAllPostsIds();
+//     return {
+//         paths,
+//         fallback: false,
+//     };
+// }
+
+// export async function getStaticProps({ params }) {
+//     const post = await getPostsData(params.id);
+//     return {
+//         props: {
+//             post,
+//         },
+//             revalidate: 3,
+//     };
+// }
+
 import { useRouter } from "next/router";
-import { getAllPostsIds, getPostsData } from "../../lib/posts";
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export default function PostData({ post }) {
-    const router = useRouter();
+  const router = useRouter();
 
-    if(router.isFallback || !post) {
-        return <div>Loading ...</div>
-    }
+  if (router.isFallback || !post) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <>
-            <h1>{post.title}</h1>
-            <p>{post.created_at}</p>
-            <p>{post.content}</p>
-        </>
-    );
+  return (
+    <div className="space-y-5 w-full">
+      <div className="flex justify-center flex-col items-center mb-5">
+        <h1 className="text-3xl mb-3 font-bold">{post.title}</h1>
+        <p className="mb-3">{post.created_at}</p>
+        <div className="border w-14"></div>
+      </div>
+      <p className="whitespace-pre-wrap">{post.content}</p>
+    </div>
+  );
 }
 
 export async function getStaticPaths() {
-    const paths = await getAllPostsIds();
-    return {
-        paths,
-        fallback: false,
-    };
+  const paths = await getAllPostIds();
+
+  return {
+    paths,
+    fallback: true,
+  };
 }
 
 export async function getStaticProps({ params }) {
-    const post = await getPostsData(params.id);
-    return {
-        props: {
-            post,
-        },
-            revalidate: 3,
-    };
+  const post = await getPostData(params.id);
+  return {
+    props: {
+      post,
+    },
+    revalidate: 3,
+  };
 }
