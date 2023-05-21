79f2c7f43b2ce18b60aa7c5421842ade427f35fa
misakiishihara <misaki.ishihara0002@gmail.com>
2023-05-21 19:21:11 +0900

    postDetailをもう少し詰める

--- pages/aboutblog.js
+++ pages/aboutblog.js
@@ -6,6 +6,21 @@ export default function Aboutblog () {
             <h1 className="text-4xl">
                 How to make this blog ?
             </h1>
+            <p>
+                このブログは以下の技術を用いて開発いたしました。
+                フロントエンド:
+                    NextJS
+                バックエンド:
+                    Django(djangorestframework)
+                DB:
+                    PostgreSQL
+                コンテナサービス:
+                    Docker
+                CodePipeline, CodeDeployによるCI/CDの自動化
+                S3による静的ホスティング、CloudFrontで配信コンテンツの最適化
+                Route53の名前解決
+                RDS
+            </p>
         </Layout>
     );
 };
