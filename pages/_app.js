4db36a275de409a1514768f7e58bc6e3b6ce42f4
{自分の名前} <misaki.ishihara0004@gmail.com>
2023-05-26 15:35:51 +0900

    analitycs tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+  <>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3JCR1Z7ZCM"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-3JCR1Z7ZCM');
+    </script>
+    <Component {...pageProps} />
+  </>
+  );
 }
 
 export default MyApp
