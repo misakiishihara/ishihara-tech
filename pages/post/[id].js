ea57ffa142bc3b57c17d8b6a88fe9c3a5fac953d
{自分の名前} <misaki.ishihara0004@gmail.com>
2023-05-26 15:16:26 +0900

    converted CMS => HTML

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,11 +1,17 @@
 import { client } from "../../lib/client";
+import Link from "next/link";
 
 export default function postId({ post }) {
   return (
-    <main>
-      <h1>{post.title}</h1>
-      <div>
-        {post.content}
+    <main className="mx-6 my-8 ">
+      <h1 className="text-3xl flex justify-center my-8">{post.title}</h1>
+      <div
+       dangerouslySetInnerHTML={{
+        __html: `${post.content}`
+       }}
+      />
+      <div className="my-10 flex justify-center opacity-50 hover:opacity-100 hover:underline">
+        <Link href="/">Back to Home</Link>
       </div>
     </main>
   );
