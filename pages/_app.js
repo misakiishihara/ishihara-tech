c7c16791dfcf99a12a405c7816a3fb12a0d512bf
{自分の名前} <misaki.ishihara0004@gmail.com>
2023-05-27 19:04:54 +0900

    error

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,8 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return( 
-  <>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3JCR1Z7ZCM"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
 
-      gtag('config', 'G-3JCR1Z7ZCM');
-    </script>
     <Component {...pageProps} />
-  </>
   );
 }
 
